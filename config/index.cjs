54cbeb8608cf5f0f8aebedd2f87706e7d7e84bac
小闰德 <89167254+ThreoSRD@users.noreply.github.com>
2022-11-24 03:58:55 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'odKn86qtAlrYe-5nyA785x_Ae56o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'A2G9mYaXqoh-ene5dZ65kiBUhIB3mNkiPuAHj7Lg-8Q',
+      useTemplateId: 'R0WtY0pufp0V6haagq2OBEDFmvWRS-CUqYbYgk9Xw1s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-09',
       festivals: [
