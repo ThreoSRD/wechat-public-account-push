69a4066c0ef81b7d97d972d0a9ad65e2a5aab1f6
小闰德 <89167254+ThreoSRD@users.noreply.github.com>
2022-11-25 16:57:09 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'odKn86qtAlrYe-5nyA785x_Ae56o',
+      id: 'odKn86tyCKa5XwCRfFMlvh-pWW58',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FMaKiK-fg_Ifu73rdvxEPdEM-UJj2eZhoz7RyELza2Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
